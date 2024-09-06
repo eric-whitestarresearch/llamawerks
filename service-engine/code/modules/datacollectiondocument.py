cb3f1c577b1892cb32c7ba4f7f59a819342934b1
Eric Moody <eric@whitestarresearch.com>
2024-09-06 12:18:59 -0500

    Added docstrings to data collection document methods

--- service-engine/code/modules/datacollectiondocument.py
+++ service-engine/code/modules/datacollectiondocument.py
@@ -20,40 +20,91 @@ from bson.objectid import ObjectId
 
 
 class DataCollectionDocument():
+  """
+  Then class for interacting with Data Collection Documents
+
+  Attributes:
+    db_client (Database): An instance of the Database class used to interact with the database
+    dcf (DataCollectionFilter): An instance of the DataCollectionFilter class used to interact with data collection filters
+  
+  """
 
   db_client = None
   dcf = None
 
   def __init__(self,db_client):
     """
-    The constructor for the ServiceComponent class.
+    The constructor for the DataCollectionDocument class.
 
     Parameters:
       self (ServiceItem): The object itself
       db_client (Str): A Database object that hosts the connection to the database
+
+    Returns:
+      None
     """
 
     self.db_client = db_client
     self.dcf =  DataCollectionFilter(self.db_client)
 
   def get_documents(self, pack_name, data_collection_name):
+    """
+    Returns all documents in data collection
+
+    Parameters:
+      pack_name (Str): The name of the pack the data collection is in
+      data_collection_name (Str): The name of the data collection that contains the document
+
+    Returns:
+      Dict: A dictonary containing the documents
+      Int: HTTP status code
+    """
     db_collection = f"{pack_name}.{data_collection_name}"
 
     db_filter = {}
     documents = self.db_client.find_all_in_collection(db_collection,db_filter)
 
-    return documents
+    return documents, 200
   
-  def get_document_with_filter(self, pack_name, data_collection_name, filter_name, filter_variables ):
+  def get_document_with_filter(self, pack_name, data_collection_name, filter_name, filter_variables):
+    """
+    Returns documents that match a data collection filter
+
+    Parameters:
+      pack_name (Str): The name of the pack the data collection is in
+      data_collection_name (Str): The name of the data collection that contains the document
+      filter_name (Str): The name of the data collection filter
+      filter_vriables (Dict): A dictonary containing the values for the variables used by the filter.
+                              Key is the name of the variable. Value is the value fo the variable
+
+    Returns
+      Dict: A dictonary containing the documents
+      Int: HTTP status code
+
+    """
     db_collection = f"{pack_name}.{data_collection_name}"
 
     db_filter = self.generate_db_filter(pack_name, filter_name, filter_variables)
     documents = self.db_client.find_all_in_collection(db_collection,db_filter)
 
-    return documents
+    return documents, 200
   
   def generate_db_filter(self, pack_name, filter_name, filter_variables):
-    
+    """
+    Generates the the filter used by the database from a data collection filter
+    Will apply the variables so it is in a form useable by the database
+
+    Parameters:
+      pack_name (Str): The name of the pack the data collection is in
+      data_collection_name (Str): The name of the data collection that contains the document
+      filter_name (Str): The name of the data collection filter
+      filter_vriables (Dict): A dictonary containing the values for the variables used by the filter.
+                              Key is the name of the variable. Value is the value fo the variable
+
+    Returns
+      Dict: A mongo database filter
+
+    """
     result = self.dcf.get_data_collection_filters(pack_name,filter_name)
     
     if result[1] == 200: #If the filter was found
@@ -67,16 +118,43 @@ class DataCollectionDocument():
       abort(404, f"Could not find the filter {filter_name} in pack {pack_name}") #The only other option is no filter was found
 
     return db_filter
-    
-
   
   def create_document(self, pack_name, data_collection_name, document):
+    """
+    Creates a document in the data collection
+
+    Parameters:
+      pack_name (Str): The name of the pack the data collection is in
+      data_collection_name (Str): The name of the data collection that contains the document
+      document (Dict): The document to add to the database
+
+    Returns:
+      Dict: Key: id value: the id of the new document
+      Int: HTTP status code
+
+    """
     db_collection = f"{pack_name}.{data_collection_name}"
     result = self.db_client.insert_document(db_collection, document)
 
     return {"id":result}, 200
   
   def update_document(self, pack_name, data_collection_name, filter_name, filter_variables, document_updates):
+    """
+    Updates documents in the data collection that match a filter
+
+    Parameters:
+      pack_name (Str): The name of the pack the data collection is in
+      data_collection_name (Str): The name of the data collection that contains the document
+      filter_name (Str): The name of the data collection filter
+      filter_vriables (Dict): A dictonary containing the values for the variables used by the filter.
+                              Key is the name of the variable. Value is the value fo the variable
+      document_updates (Dict): A dictonary containing the updates to the document
+
+    Returns:
+      Dict: Key: updated, value: a count of the updated documents
+      Int: HTTP status code
+    
+    """
     db_filter = self.generate_db_filter(pack_name, filter_name, filter_variables)
     db_collection = f"{pack_name}.{data_collection_name}"
 
@@ -85,14 +163,41 @@ class DataCollectionDocument():
     return {"updated" : result}, 200
 
   def get_document_by_id(self, pack_name, data_collection_name, document_id):
+    """
+    Returns document that matches the document id
+
+    Parameters:
+      pack_name (Str): The name of the pack the data collection is in
+      data_collection_name (Str): The name of the data collection that contains the document
+      document_id (Str): The document ID. This is a 24 character hex string that is the _id in the mongo database
+
+    Returns
+      Dict: A dictonary containing the documents
+      Int: HTTP status code
+
+    """
     db_collection = f"{pack_name}.{data_collection_name}"
 
     db_filter = {"_id": ObjectId(document_id)}
     documents = self.db_client.find_one_in_collection(db_collection,db_filter)
 
-    return documents
+    return documents, 200
   
   def update_document_by_id(self, pack_name, data_collection_name, document_id, document):
+    """
+    Updates document that matches the document id
+
+    Parameters:
+      pack_name (Str): The name of the pack the data collection is in
+      data_collection_name (Str): The name of the data collection that contains the document
+      document_id (Str): The document ID. This is a 24 character hex string that is the _id in the mongo database
+      document (Dict): A dictonary containing the updates to the document
+
+    Returns:
+      Dict: Key: updated, value: a count of the updated documents
+      Int: HTTP status code
+    
+    """
     db_collection = f"{pack_name}.{data_collection_name}"
     db_filter = {"_id": ObjectId(document_id)}
 
@@ -101,6 +206,19 @@ class DataCollectionDocument():
     return {"updated" : result}, 200
   
   def delete_document_by_id(self, pack_name, data_collection_name, document_id ):
+    """
+    Delete document that matches the document id
+
+    Parameters:
+      pack_name (Str): The name of the pack the data collection is in
+      data_collection_name (Str): The name of the data collection that contains the document
+      document_id (Str): The document ID. This is a 24 character hex string that is the _id in the mongo database
+    
+    Returns:
+      Dict: Key: deleted, value: a count of the deleted documents
+      Int: HTTP status code
+    
+    """
     db_collection = f"{pack_name}.{data_collection_name}"
     db_filter = {"_id": ObjectId(document_id)}
 
